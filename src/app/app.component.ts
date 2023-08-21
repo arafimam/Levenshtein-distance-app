d56c22715f145de6f4b2cc36404b4e553511da84
arafimam -Git <syedarafimam27@gmail.com>
2023-08-20 20:46:14 -0700

    Implemented the Levenshtein algorithm and front end UI with dummy data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Product} from "./product";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,79 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'searchProducts';
+  searched: string = '';
+  typingTimer: any;
+  typingTimeout = 1000;
+  products: Product[] = [
+    {
+      name: "Laptop",
+      image: "/assets/laptop.jpg",
+      price: "$275"
+    },
+    {
+      name: "Playstation",
+      image: "/assets/PS4.png",
+      price: "300$"
+    },
+    {
+      name: "Football",
+      image: "/assets/football.jpg",
+      price: "75$"
+    },
+    {
+      name: "Basketball",
+      image: "/assets/basketball.jpg",
+      price: "50$"
+    }
+  ]
+  productsToDisplay: Product[] = this.products;
+  isSearching: boolean = false;
+
+  onSearchChanged(){
+    if (this.searched !== ""){
+      this.productsToDisplay = [];
+      clearTimeout(this.typingTimer);
+      this.isSearching = true;
+      this.typingTimer = setTimeout(() => {
+        this.productsToShow(this.searched);
+        this.isSearching = false;
+      }, this.typingTimeout);
+    }
+    else{
+      this.productsToDisplay = this.products;
+    }
+
+  }
+
+  private productsToShow(searchWord: string){
+    this.products.forEach(product => {
+      var score = this.getWordDistance(searchWord.toLowerCase(),product.name.toLowerCase());
+      if(score <= 5 ){
+        this.productsToDisplay.push(product);
+      }
+    })
+  }
+
+  private getWordDistance(word1: string, word2: string): number{
+    const numberOfRows: number = word2.length + 1;
+    const numberOfColumns: number = word1.length + 1;
+    const levenshteinGraph: number[][] = new Array(numberOfRows).fill([]).map(() => new Array(numberOfColumns).fill(0));
+
+    for (let i = 0; i < numberOfRows; i++) {
+      for (let j = 0; j < numberOfColumns; j++) {
+        if (Math.min(i, j) === 0) {
+          levenshteinGraph[i][j] = Math.max(i, j);
+        } else {
+          const characterOfWord1: string = word1.charAt(j - 1);
+          const characterOfWord2: string = word2.charAt(i - 1);
+          const deleteCost: number = levenshteinGraph[i - 1][j] + 1;
+          const insertionCost: number = levenshteinGraph[i][j - 1] + 1;
+          const replaceCost: number = levenshteinGraph[i - 1][j - 1] + (characterOfWord1 !== characterOfWord2 ? 1 : 0);
+          const minimumCost: number = Math.min(deleteCost, insertionCost, replaceCost);
+          levenshteinGraph[i][j] = minimumCost;
+        }
+      }
+    }
+    return levenshteinGraph[numberOfRows - 1][numberOfColumns - 1];
+  }
 }
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,12 @@
+import {Component, Input} from '@angular/core';
+import {Product} from "../product";
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent {
+  @Input() product: Product = {name: "Laptop", image: "/assets/laptop.jpg", price: "240$"};
+
+}
